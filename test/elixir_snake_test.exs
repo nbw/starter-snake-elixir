7b1353629ec0e0f744a8e461d3315628928e0e7b
Mark Zsombor <markszsombor@gmail.com>
2018-10-06 17:03:41 -0700

    Added tests

--- test/elixir_snake_test.exs
+++ test/elixir_snake_test.exs
@@ -2,7 +2,24 @@ defmodule ElixirSnakeTest do
   use ExUnit.Case
   doctest ElixirSnake
 
-  test "greets the world" do
-    assert ElixirSnake.hello() == :world
+  test "responds to start request" do
+    start_resp = ElixirSnake.start_resp(1)
+    assert is_map(start_resp)
   end
+
+  test "start response has color property" do
+    start_resp = ElixirSnake.start_resp(1)
+    assert Map.has_key?(start_resp, :color)
+  end
+
+  test "responds to move request" do
+    move_resp = ElixirSnake.move_resp(1)
+    assert is_map(move_resp)
+  end
+
+  test "move response has move property" do
+    move_resp = ElixirSnake.move_resp(1)
+    assert Map.has_key?(move_resp, :move)
+  end
+
 end
